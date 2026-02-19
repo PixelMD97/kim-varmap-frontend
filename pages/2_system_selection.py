519ccbca3c706a54d203c323e2b8db4d82d56dd8
PixelMD97 <sarah.deckarm1@gmail.com>
2026-02-19 08:32:02 +0100

    Update 2_system_selection.py

--- pages/2_system_selection.py
+++ pages/2_system_selection.py
@@ -2,8 +2,7 @@ import streamlit as st
 
 from ui_stepper import render_stepper, render_bottom_nav
 from auth_ui import render_auth_status
-from api_client import update_project_settings
-from project_guard import require_project
+from api_client import update_project_settings 
 
 
 if "project" not in st.session_state:
